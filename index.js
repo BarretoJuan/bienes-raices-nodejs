fa189d82df86dba53d54e41bcd79bf2441b974df
Juan Barreto <juan2.manuelbarreto@gmail.com>
2023-08-01 23:24:08 -0400

    first step

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log(5+100);
+const express = require('express')
+
+const app = express()
+
+
+app.get("/", function(req,res) {
+    res.send("Hola Mundo en Express")
+})
+
+
+// Definir puerto
+
+const port = 3000;
+app.listen(port, () => {
+    console.log("El server está funcionando en el puerto ${port}")
+});
+
