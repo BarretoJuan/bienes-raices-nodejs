fa4272113c60b4f88888d941666746ea8080512e
Juan Barreto <juan2.manuelbarreto@gmail.com>
2023-08-04 13:10:50 -0400

    Added first controller to the mvc architecture

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,13 +1,10 @@
 import express from "express";
+import {loginForm} from "../controllers/userController.js"
 
 const router = express.Router();
 
 // Routing
-router.get('/login', (req, res) => {
-    res.render('auth/login', {
-        authenticated: false
-    })
-});
+router.get('/login', loginForm);
 
 router.get('/about-us', function(req,res) {
     res.send('Info About us')
