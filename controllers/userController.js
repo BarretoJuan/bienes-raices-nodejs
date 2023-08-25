c0648fb5ad3766e49c15f9c4fa26c270670212f6
Juan Barreto <juan2.manuelbarreto@gmail.com>
2023-08-25 13:04:02 -0400

    Added duplicate email validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,7 +23,7 @@ const registerAction = async (req,res) => {
     await check('nombre').notEmpty().withMessage("El nombre no puede estar vacío").run(req)
     await check('email').isEmail().withMessage("El email debe ser una direccion email válida").run(req)
     await check('password').isLength({min:6, max:255}).withMessage("La contraseña debe tener minimo 6 y maximo 255 caracteres").run(req)
-    await check('repetir_password').equals("password").withMessage("Las contraseñas no son iguales").run(req)
+    await check('repetir_password').equals(req.body.password).withMessage("Las contraseñas no son iguales").run(req)
     let resultado = validationResult(req)
 
     // verificar que resultado esté vacio
@@ -37,6 +37,19 @@ const registerAction = async (req,res) => {
                 }
     })
 
+    }
+
+    // Verificar que el usuario no esté duplicado
+    const existeUsuario = await Usuario.findOne( { where : {email : req.body.email}})
+    if (existeUsuario) {
+        return res.render('auth/register', {
+            authenticated: false,
+            pagina : "Crear cuenta",
+            errores : [{msg: "Este usuario ya está registrado"}],
+            usuario : {nombre: req.body.nombre,
+                       email : req.body.email,
+                    }
+        })
     }
 
     const usuario = await Usuario.create(req.body)
